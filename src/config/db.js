ea5be42f193fe4695886d6fcae426b338b6b1d99
gsshakthivel <gsshkvl@gmail.com>
2026-09-20 10:40:20 +0530

    Support MySQL SSL configuration

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,5 @@
 import mysql from "mysql2/promise";
+import fs from "fs";
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -6,6 +7,12 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
+    ssl: process.env.DB_SSL === "true"
+        ? {
+            ca: fs.readFileSync(process.env.DB_SSL_CA)
+        }
+        : undefined
 });
 
 export default pool;
